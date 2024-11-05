0cfc7c132a00905deac2ea2b175a3335d4db1cfe
SBJ <swissbankingjew@proton.me>
2024-11-05 15:40:24 +0100

    Create a start-server function for deployment

--- main.lisp
+++ main.lisp
@@ -1,23 +1,32 @@
 (defpackage :homepage
   (:use :cl
-   :hunchentoot))
+   :hunchentoot)
+  (:export :start-server))
 
 (in-package :homepage)
 
-(defparameter *server* (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242)))
+(defun start-server ()
+  (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242)))
 
+(defparameter *server* (start-server))
 
 (defun nav-bar ()
   (spinneret:with-html
-    `(:div :class "flex flex-row"
-	   (:div :class "basis-1/4"
-		 (:h5 "Home"))
-	   (:div :class "basis-1/4"
-		 (:h5 "Page"))
-	   (:div :class "basis-1/4"
-		 (:h5 "Stuff"))
-	   (:div :class "basis-1/4"
-		 (:h5 "Coding")))))
+    `(:nav :class "bg-gray-800 text-gray-100 p-4"
+	   (:ul :class "flex space-x-6"
+		(:li (:a :href "/" :class "hover:text-gray-300" "Home"))
+		(:li (:a :href "/about" :class "hover:text-gray-300" "About"))
+		(:li (:a :href "/projects" :class "hover:text-gray-300" "Projects"))
+		(:li (:a :href "/contact" :class "hover:text-gray-300" "Contact"))
+		))))
+
+(defvar *terracotta* "#E2725B")
+
+(defun button (text &optional on-click)
+  (spinneret:with-html
+    `(:button :class "bg-blue-500 hover:bg-deepRed text-cream px-4 py-2 rounded"
+	      ,text)))
+
 
 (defmacro with-page ((&key title) &body body)
   `(spinneret:with-html-string
@@ -25,16 +34,27 @@
      (:html
       (:head
        (:script :src "https://unpkg.com/htmx.org@2.0.3")
-       (:script :src "https://cdn.tailwindcss.com"))      
-      (:body
-       ,(nav-bar)
-       ,@body))))
+       (:script :src "https://cdn.tailwindcss.com"))
+      (:body :class "container mx-auto px-4"
+	     ,(nav-bar)
+	     ,@body))))
 
 (hunchentoot:define-easy-handler (say-yo :uri "/") ()
   (setf (hunchentoot:content-type*) "text/html")  
-  (with-page (:title "Title") (:h1 "Hello")))
+  (with-page (:title "Title")
+    (:h1 :class "text-6xl" "Whale Cum to my page")))
 
-(hunchentoot:define-easy-handler (clicked :uri "/clicked") ()
+(hunchentoot:define-easy-handler (about :uri "/about") ()
   (setf (hunchentoot:content-type*) "text/html")
-  (spinneret:with-html-string
-    (:h1 "THE GREAT REPLACEMENT!")))
+  (with-page (:title "About")
+    (:h1 :class "text-4xl" "WIP")))
+
+(hunchentoot:define-easy-handler (projects :uri "/projects") ()
+  (setf (hunchentoot:content-type*) "text/html")
+  (with-page (:title "Projects")
+    (:h1 :class "text-4xl" "WIP")))
+
+(hunchentoot:define-easy-handler (contact :uri "/contact") ()
+  (setf (hunchentoot:content-type*) "text/html")
+  (with-page (:title "Contact")
+    (:h1 :class "text-4xl" "WIP")))
